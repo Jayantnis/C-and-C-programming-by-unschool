e2a0493b4e0c20e0fdc746c59e80ab71d4be3bbb
Jayantnis <65399679+Jayantnis@users.noreply.github.com>
2020-05-16 18:57:41 +0530

    Add files via upload
    Ternary operator program by jayant nishad

---
+++ tarnaryoperator.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//Ternary operator is (symbole uses:-  ?  : ) or conditional operators
+//syntax:- variable =(condition)? value1:value2
+
+int main()
+{ //explain:-
+	int a=2,big,b=3;
+big=(a>b)?a:b;//b=3 in largest no. so big=3 ans.=3
+printf("value of big:- %d",big);	
+	//so
+/*	if(a>b)
+	{
+		big=a;
+	}
+	else 
+	 big=b;
+*/
+//this type of uses
+
+}
